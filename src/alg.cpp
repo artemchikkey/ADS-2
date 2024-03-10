d7373f206c201f3f8b3569d1175afc16c9319db8
artemchikkey <160394124+artemchikkey@users.noreply.github.com>
2024-03-10 21:03:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,8 @@
 
 
 double pown(double value, uint16_t n) {
-  double x=0;
-  x=pow(value, n);
+  double x = 0;
+  x = pow(value, n);
   return x;
 }
 
@@ -17,11 +17,11 @@ return x;
 }
 
 double calcItem(double x, uint16_t n) {
- return (pow(x, n) / fact(n));
+return (pow(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
- double c = 0;
+double c = 0;
     for (uint64_t i = 0; i <= count; i++) {
         c += (pow(x, i) / fact(i));
     }
@@ -31,13 +31,13 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
 double result = 0.0;
 for (uint16_t i = 1; i <= count; i++)
-  result = result + pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  result = result + pow(-1, i - 1) * calcItem(x, 2 * i - 1);
 return result;
 }
 
 double cosn(double x, uint16_t count) {
 double result = 0.0;
 for (uint16_t i = 1; i <= count; i++)
-  result = result + pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  result = result + pow(-1, i - 1) * calcItem(x, 2 * i - 2);
 return result;
 }
